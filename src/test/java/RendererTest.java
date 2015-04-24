8dd8fb28ac0ecc9bfb2abf38e1a170b1fe2c1745
kerrin <kerrinenglish@gmail.com>
2015-04-24 21:35:45 +1000

    add tests for Modeler and Renderer

---
+++ src/test/java/RendererTest.java
@@ -0,0 +1,17 @@
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class RendererTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
