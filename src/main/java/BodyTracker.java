615c67c8a18f275fb81d58960123466b4818b7e3
kerrin <kerrinenglish@gmail.com>
2015-05-23 23:06:11 +1000

    (BodyTracker) set stage size

--- src/main/java/BodyTracker.java
+++ src/main/java/BodyTracker.java
@@ -1,25 +1,23 @@
 import javafx.application.Application;
-import javafx.embed.swing.SwingNode;
-import javafx.scene.Group;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-
-import javax.swing.*;
-
 
 public class BodyTracker extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	/**
+	 * This is the entry point for a JavaFX application. The element that
+	 * encapsulates the entire app is the stage.
+	 */
 	@Override
 	public void start(Stage stage) throws Exception {
-
-
 		// The model for the renderer
 		Modeler modeler = new Modeler();
 		// The view for the renderer
@@ -35,7 +33,8 @@ public class BodyTracker extends Application {
         // The controller for the renderer (needs to be initialize here as it is initial view)
         Renderer rendererController = new Renderer(modeler, rendererView);
 
-        // Configure scene
+        // Configure stage
+		setStageSize(stage);
         stage.setOnCloseRequest(event -> {
             // Unmount the Renderer before app closure.
             // TODO track the actual current view.
@@ -44,6 +43,20 @@ public class BodyTracker extends Application {
         stage.setTitle("ClothMotion");
         stage.setScene(scene);
 		stage.show();
+	}
+
+	/**
+	 * Set the size of the specified stage. The size will be set to 1280x720
+	 * unless the usable screen size is smaller than this. If the screen size
+	 * is smaller than 1280x720, the stage will be set equal to screen size.
+	 * @param stage The stage to set the size of
+	 */
+	private void setStageSize(Stage stage) {
+		Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+		double width = Math.min(1280, screen.getWidth());
+		double height = Math.min(720, screen.getHeight());
+		stage.setWidth(width);
+		stage.setHeight(height);
 	}
 
 	// Can be useful to have a separate tab to review history/previous drawings
