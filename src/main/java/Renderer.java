61945ef090869f8eac62e1b5b1c9abdd26e8f5db
kerrin <kerrinenglish@gmail.com>
2015-05-24 10:15:00 +1000

    (Sample) Add constructor to take all properties

--- src/main/java/Renderer.java
+++ src/main/java/Renderer.java
@@ -292,4 +292,14 @@ class Sample {
     public Double roll;
     public Double yaw;
     public Double pitch;
+
+    public Sample() {}
+
+    public Sample(int sensorId, long timestamp, Double roll, Double yaw, Double pitch) {
+        this.sensorId = sensorId;
+        this.timestamp = timestamp;
+        this.roll = roll;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
 }
