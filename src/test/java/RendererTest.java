13f9bb7935bc32a40e5171b68cc55ec468ee83fb
kerrin <kerrinenglish@gmail.com>
2015-05-25 16:54:13 +1000

    (Renderer) update message parser test

--- src/test/java/RendererTest.java
+++ src/test/java/RendererTest.java
@@ -21,16 +21,16 @@ public class RendererTest {
     // should parse single line message correctly
     @Test
     public void parseSingleLineMessage() {
-        String msg = "id 55 time 9299 x 359.9375 y -39.8750 z -22.6250\n";
+        String msg = "id 55 time 25262 x 352.6250 y -0.5625 z -100.1875 \n";
         List<Sample> samples = Renderer.parseMessage(msg);
 
         assertEquals(1, samples.size());
         Sample sample = samples.get(0);
         assertEquals(55, sample.sensorId);
-        assertEquals(9299, sample.timestamp);
-        assertEquals(359.9375, sample.yaw, 0.01);
-        assertEquals(-39.8750, sample.pitch, 0.01);
-        assertEquals(-22.6250, sample.roll, 0.01);
+        assertEquals(25262, sample.timestamp);
+        assertEquals(352.6250, sample.yaw, 0.01);
+        assertEquals(-0.5625, sample.pitch, 0.01);
+        assertEquals(-100.1875, sample.roll, 0.01);
     }
 
     // should parse multiline message correctly
